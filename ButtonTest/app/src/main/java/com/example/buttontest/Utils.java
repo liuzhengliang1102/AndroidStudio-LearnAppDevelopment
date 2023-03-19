f43384f289aeac748f33c65e7f24f1e291304ffb
liuzhengliang1102 <liuzl.1987@163.com>
2023-03-19 20:42:52 +0800

    add ViewAttributesTest  TextViewTest  ButtonTest  source code
    1. add ViewAttributesTest code
    2. add TextViewTest code
    3. add ButtonTest code

---
+++ ButtonTest/app/src/main/java/com/example/buttontest/Utils.java
@@ -0,0 +1,41 @@
+package com.example.buttontest;
+
+import android.content.Context;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Utils {
+    // 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+    public static int dp2px(Context context, float dp) {
+        // 获取当前手机的像素密度（1个dp对应几个px）
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f); // 四舍五入取整
+    }
+
+    // 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+    public static int px2dp(Context context, float px) {
+        // 获取当前手机的像素密度（1个dp对应几个px）
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (px / scale + 0.5f); // 四舍五入取整
+    }
+
+    // 获取当前的日期时间
+    public static String getNowDateTime() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return simpleDateFormat.format(new Date());
+    }
+
+    // 获取当前的时间
+    public static String getNowTime() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+        return simpleDateFormat.format(new Date());
+    }
+
+    // 获取当前的时间(毫秒ms)
+    public static String getNowTimeMs() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss:SSS");
+        return simpleDateFormat.format(new Date());
+    }
+}
+
